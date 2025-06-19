d1856f5e1ffaf7bd461a554f7ceffa28f56f882c
Ashley Doering <Ashley.doering@outlook.com>
2025-06-19 19:53:46 -0400

    added new code variables  to my Program.cs

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Ashley";
+            int age = 36;
+            double avg = 98.7;
+            decimal bankAccount = 10000.00m;
+            char initial = 'S';
+            bool isMarried = true;
+            
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nAverage: {avg}\nMoney: {bankAccount}\nInitial: {initial}\nIsMarried: {isMarried}");
         }
     }
 }
